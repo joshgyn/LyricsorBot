f0cb17c8e8e3cdbd598d182aab82efeddecabea9
root <root@production-builder.us-west1-b.c.railway-infra.internal>
2021-08-13 10:40:40 +0000

    Initial Commit

---
+++ utils/configs.py
@@ -0,0 +1,66 @@
+import os
+import time
+
+
+class Var(object):
+
+    # Get a bot token from botfather
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+
+    # Get from my.telegram.org
+    API_ID = int(os.environ.get("API_ID", 12345))
+
+    # Get from my.telegram.org
+    API_HASH = os.environ.get("API_HASH", "")
+
+    # ID of users that can't use the bot commands
+    BANNED_USERS = set(
+        int(x) for x in os.environ.get(
+            "BANNED_USERS", "").split())
+
+    # To record start time of bot
+    BOT_START_TIME = time.time()
+
+    # Genius Api From Here : https://genius.com/api-clients
+    API = os.environ.get("GENIUS_API", None)
+
+    # buttons
+    PAGENUM = int(os.environ.get("PAGENUM", 20))
+
+
+class Tr(object):
+
+    START_TEXT = """
+Hi {} !
+Welcome To @LyricsorBot !
+
+Lyricsor Can Help You Get Song Lyrics
+"""
+
+    ABOUT_TEXT = """🤖 **My Name:** [Lyricsor](t.me/LyricsorBot)
+
+📝 **Language:** [Python 3](https://www.python.org)
+
+📚 **Framework:** [Pyrogram](https://github.com/pyrogram/pyrogram)
+
+📡 **Hosted On:** [Railway](Railway.app)
+
+👨‍💻 **Developer:** [Enjoyer](t.me/FeelDeD)
+"""
+
+    HELP_TEXT = """💡 Just Send Me The Name Of The Song, That's it
+"""
+
+    ERR_TEXT = "⚠️ Genius API Not Found"
+
+    ERRTOKEN_TEXT = "😶 The Access Token Provided Is Expired, Revoked, Malformed Or Invalid For Other Reasons.",
+
+    NORES = "💬 No Results"
+
+    SEARCHING = "🔍 Searching For:"
+
+    WAIT = "💬 Please Wait !!"
+
+    ARTIST = "🗣 Artist:"
+
+    SONG = "🎵 Song:"
